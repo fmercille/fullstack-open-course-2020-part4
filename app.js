9bf1d4ac57c0e7566b4895c0b3a0957375e34e54
Frederic Mercille <fmercille@gmail.com>
2020-07-19 21:33:18 -0400

    Move the app into its own app module

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const mongoUrl = config.MONGODB_URI
+logger.info('Connecting to', mongoUrl)
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.info('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+morgan.token('body', function (req) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
