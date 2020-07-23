50135960f1c423b9f466cc742471c3d8966ee0b9
Frederic Mercille <fmercille@gmail.com>
2020-07-22 22:20:00 -0400

    Exercise 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -26,6 +26,11 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('unique identifier property is named id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
